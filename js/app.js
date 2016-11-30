c34974d8ad4e40ff09f8d9d5d841bd7f1702325f
Felix Su <felixs8696@berkeley.edu>
2016-11-30 12:54:23 -0800

    added scroll on state change

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 (function($) {
   angular.module('MB', ['ui.router', 'ui.materialize', 'ngAnimate'])
-  .run(['$state', function($state) {
-    console.log($state.params);
+  .run(['$rootScope', function($rootScope) {
+    $rootScope.$on('$stateChangeSuccess', function() {
+       if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0)
+        setTimeout(() => {$('html, body').animate({scrollTop:0}, 300)}, 0);
+    });
   }])
   .constant('Dropbox', new Dropbox({ accessToken: 'jxFO4XmR3oAAAAAAAAAADo6UZ3wEVJac19ppbs7teOK0kOuzfHIa1xvBID-FxSkG' }))
   .constant('ContactSheetURL', 'https://script.google.com/macros/s/AKfycbxr8GYXt7yPoqAYZJIwyZcYYyOAW-7gPiFCPOBr2J0ccMQcpGU/exec')
