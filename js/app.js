9dad5b684464148a38c807fe4026f8d23aa95828
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-12-23 01:37:51 -0600

    Updated application URLS

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
     });
   }])
   .constant('Dropbox', new Dropbox({ accessToken: 'jxFO4XmR3oAAAAAAAAAADo6UZ3wEVJac19ppbs7teOK0kOuzfHIa1xvBID-FxSkG' }))
-  .constant('ContactSheetURL', 'https://script.google.com/macros/s/AKfycbxr8GYXt7yPoqAYZJIwyZcYYyOAW-7gPiFCPOBr2J0ccMQcpGU/exec')
-  .constant('ApplicationSheetURL', 'https://script.google.com/macros/s/AKfycbz-BimCOd0hicCGaz8GfKmAlYb9H21JEbs8WYMHlPcY_ctzIBw/exec')
-  .constant('CompanySheetURL', 'https://script.google.com/macros/s/AKfycbyCk_a4ln1zY8QWuXz7IzjsidV9_2Ubp8mym9TOaL2i5iBw7ndc/exec');
+  .constant('ContactSheetURL', 'https://script.google.com/macros/s/AKfycbwgfI7poKThVxhWtYTDCqAKJw5oqo_6sJMR46EGaoGiKZ92VRG-/exec')
+  .constant('ApplicationSheetURL', 'https://script.google.com/macros/s/AKfycbyWPGobTkBaiFxAqRsO4RgbVTqarcCPUm0fE4yZUGsv4ZsJR3k/exec')
+  .constant('CompanySheetURL', 'https://script.google.com/macros/s/AKfycbz94-rrAQFMYWIi98g96MZVF7pk6K0AIjJM7PzjH0NzJI7ZiA3g/exec');
 })(jQuery);
--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
         $('html, body').animate({ scrollTop: 0 }, 300);
       }, 0);
     });
-  }]).constant('Dropbox', new Dropbox({ accessToken: 'jxFO4XmR3oAAAAAAAAAADo6UZ3wEVJac19ppbs7teOK0kOuzfHIa1xvBID-FxSkG' })).constant('ContactSheetURL', 'https://script.google.com/macros/s/AKfycbxr8GYXt7yPoqAYZJIwyZcYYyOAW-7gPiFCPOBr2J0ccMQcpGU/exec').constant('ApplicationSheetURL', 'https://script.google.com/macros/s/AKfycbz-BimCOd0hicCGaz8GfKmAlYb9H21JEbs8WYMHlPcY_ctzIBw/exec').constant('CompanySheetURL', 'https://script.google.com/macros/s/AKfycbyCk_a4ln1zY8QWuXz7IzjsidV9_2Ubp8mym9TOaL2i5iBw7ndc/exec');
+  }]).constant('Dropbox', new Dropbox({ accessToken: 'jxFO4XmR3oAAAAAAAAAADo6UZ3wEVJac19ppbs7teOK0kOuzfHIa1xvBID-FxSkG' })).constant('ContactSheetURL', 'https://script.google.com/macros/s/AKfycbwgfI7poKThVxhWtYTDCqAKJw5oqo_6sJMR46EGaoGiKZ92VRG-/exec').constant('ApplicationSheetURL', 'https://script.google.com/macros/s/AKfycbyWPGobTkBaiFxAqRsO4RgbVTqarcCPUm0fE4yZUGsv4ZsJR3k/exec').constant('CompanySheetURL', 'https://script.google.com/macros/s/AKfycbz94-rrAQFMYWIi98g96MZVF7pk6K0AIjJM7PzjH0NzJI7ZiA3g/exec');
 })(jQuery);
 'use strict';
 
@@ -287,28 +287,6 @@
 'use strict';
 
 (function () {
-  angular.module('MB').controller('ContactCtrl', ContactCtrl);
-  ContactCtrl.$inject = ['FormService', '$http', '$log', 'ContactSheetURL'];
-
-  function ContactCtrl(FormService, $http, $log, ContactSheetURL) {
-    var vm = this;
-
-    vm.submitted = false;
-    vm.contact = { firstName: null, lastName: null, email: null, subject: null, message: null };
-
-    vm.sendMessage = function () {
-      var sent = FormService.sendToSheet(vm.contact, ContactSheetURL);
-      if (sent) {
-        vm.submitted = true;
-        return true;
-      }
-      return false;
-    };
-  }
-})();
-'use strict';
-
-(function () {
   angular.module('MB').controller('HomeCtrl', HomeCtrl);
 
   function HomeCtrl() {
@@ -344,5 +322,27 @@
       subheader: 'CMG Consulting',
       image: path + 'michellehuang.jpg'
     }];
+  }
+})();
+'use strict';
+
+(function () {
+  angular.module('MB').controller('ContactCtrl', ContactCtrl);
+  ContactCtrl.$inject = ['FormService', '$http', '$log', 'ContactSheetURL'];
+
+  function ContactCtrl(FormService, $http, $log, ContactSheetURL) {
+    var vm = this;
+
+    vm.submitted = false;
+    vm.contact = { firstName: null, lastName: null, email: null, subject: null, message: null };
+
+    vm.sendMessage = function () {
+      var sent = FormService.sendToSheet(vm.contact, ContactSheetURL);
+      if (sent) {
+        vm.submitted = true;
+        return true;
+      }
+      return false;
+    };
   }
 })();
