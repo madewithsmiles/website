91a00f5cf1b691445f46230482bc9f9d694b61fe
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-02-16 20:35:42 -0800

    Added competitive programming template

---
+++ template.cpp
@@ -0,0 +1,51 @@
+// Importing standard libraries
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <functional>
+using namespace std;
+
+// To learn more, search "macros in c++"
+#define rep(i,a,n) for(int i=a;i<n;i++)
+#define mp make_pair
+#define pb push_back
+#define sz(x) ((int) (x).size())
+#define all(x) (x).begin(),(x).end()
+#define clr(x) (memset((x),0,sizeof(x)))
+
+// Global variables here
+int test;
+
+// I/O Functions
+void input() {
+
+}
+
+void output() {
+
+}
+
+// Begin solution
+
+void solve() {
+
+}
+
+// End solution
+
+// Modify based on input/output format!!!
+int main() {
+    cin >> test;
+    while (test--) {
+        input();
+        solve();
+        output();
+    }
+    return 0;
+}
