fc9909373b55ce8beccdcf69dc9d2336198beed6
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-02-28 17:50:33 -0800

    Updated script

--- js/script.js
+++ js/script.js
@@ -175,6 +175,54 @@
 'use strict';
 
 (function () {
+  angular.module('MB').factory('LabService', LabService);
+
+  function LabService() {
+    var factory = {
+      getExperimentMetaData: getExperimentMetaData,
+      getExperimentData: getExperimentData
+    };
+
+    var experimentMetaData = [{
+      titlePath: "glasses",
+      title: "Glasses with Face Recognition",
+      authors: "Peter Lee",
+      tags: ["Project Sherlock", "Computer Vision"],
+      category: "Computer Vision",
+      preview: "Experimenting with face model fitting."
+    }];
+
+    function parseText(text) {
+      // Trim whitespace
+      return text.replace(/^ +| +$/gm, "");
+    }
+
+    function cleanExperimentData(experiment) {
+      experiment.preview = parseText(experiment.preview);
+      return experiment;
+    }
+
+    function getExperimentMetaData() {
+      var cleanData = experimentMetaData;
+      for (var i = 0; i < cleanData.length; i++) {
+        cleanData[i].preview = parseText(cleanData[i].preview);
+      }
+      return cleanData;
+    }
+
+    function getExperimentData(titlePath) {
+      for (var i = 0; i < experimentMetaData.length; i++) {
+        if (titlePath == experimentMetaData[i].titlePath) return cleanExperimentData(experimentMetaData[i]);
+      }
+      return null;
+    }
+
+    return factory;
+  }
+})();
+'use strict';
+
+(function () {
   angular.module('MB').factory('FormService', FormService);
 
   FormService.$inject = ['$http', '$log', 'Dropbox'];
@@ -320,54 +368,6 @@
       }
 
       if (vmObject[textObject][textKey].length == 0) vmObject[wordCountVar] = 0;
-    }
-
-    return factory;
-  }
-})();
-'use strict';
-
-(function () {
-  angular.module('MB').factory('LabService', LabService);
-
-  function LabService() {
-    var factory = {
-      getExperimentMetaData: getExperimentMetaData,
-      getExperimentData: getExperimentData
-    };
-
-    var experimentMetaData = [{
-      titlePath: "glasses",
-      title: "Glasses with Face Recognition",
-      authors: "Peter Lee",
-      tags: ["Project Sherlock", "Computer Vision"],
-      category: "Computer Vision",
-      preview: "Experimenting with face model fitting."
-    }];
-
-    function parseText(text) {
-      // Trim whitespace
-      return text.replace(/^ +| +$/gm, "");
-    }
-
-    function cleanExperimentData(experiment) {
-      experiment.preview = parseText(experiment.preview);
-      return experiment;
-    }
-
-    function getExperimentMetaData() {
-      var cleanData = experimentMetaData;
-      for (var i = 0; i < cleanData.length; i++) {
-        cleanData[i].preview = parseText(cleanData[i].preview);
-      }
-      return cleanData;
-    }
-
-    function getExperimentData(titlePath) {
-      for (var i = 0; i < experimentMetaData.length; i++) {
-        if (titlePath == experimentMetaData[i].titlePath) return cleanExperimentData(experimentMetaData[i]);
-      }
-      return null;
     }
 
     return factory;
