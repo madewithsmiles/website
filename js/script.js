468b1510522aa655713e68719b60228c6ae2327b
visatish <vishalstw@gmail.com>
2017-03-08 19:34:38 -0800

    Final commit on this branch

--- js/script.js
+++ js/script.js
@@ -130,32 +130,6 @@
 'use strict';
 
 (function () {
-  angular.module('MB').factory('DropboxService', DropboxService);
-
-  DropboxService.$inject = ['Dropbox', '$http', '$log'];
-
-  function DropboxService(Dropbox, $http, $log) {
-    var factory = {
-      uploadFile: uploadFile
-    };
-
-    function uploadFile(filePath, fileContents) {
-      Dropbox.filesUpload({ path: filePath, contents: fileContents, mode: { ".tag": "add" }, autorename: true }).then(function (response) {
-        $log.debug('File Uploaded to Dropbox: ' + JSON.stringify(response));
-        return true;
-      }).catch(function (error) {
-        $log.error(error);
-        return false;
-      });
-      return true;
-    }
-
-    return factory;
-  }
-})();
-'use strict';
-
-(function () {
     angular.module('MB').factory('DateService', DateService);
 
     DateService.$inject = ['moment'];
@@ -175,51 +149,24 @@
 'use strict';
 
 (function () {
-  angular.module('MB').factory('LabService', LabService);
+  angular.module('MB').factory('DropboxService', DropboxService);
 
-  function LabService() {
+  DropboxService.$inject = ['Dropbox', '$http', '$log'];
+
+  function DropboxService(Dropbox, $http, $log) {
     var factory = {
-      getExperimentMetaData: getExperimentMetaData,
-      getExperimentData: getExperimentData
+      uploadFile: uploadFile
     };
 
-    var experimentMetaData = [{
-      titlePath: "glasses",
-      title: "Glasses with Face Recognition",
-      tags: ["Project Sherlock", "Computer Vision"],
-      category: "Computer Vision",
-      preview: "Experimenting with Haar Cascades for real-time face detection."
-    }, {
-      titlePath: "interactive-physics-engine",
-      title: "Interactive Physics Engine",
-      tags: ["Javascript", "Computer Vision", "Physics-Engine"],
-      category: "Computer Vision",
-      preview: "Experimenting with javascript physics engine and hand tracking."
-    }];
-
-    function parseText(text) {
-      // Trim whitespace
-      return text.replace(/^ +| +$/gm, "");
-    }
-
-    function cleanExperimentData(experiment) {
-      experiment.preview = parseText(experiment.preview);
-      return experiment;
-    }
-
-    function getExperimentMetaData() {
-      var cleanData = experimentMetaData;
-      for (var i = 0; i < cleanData.length; i++) {
-        cleanData[i].preview = parseText(cleanData[i].preview);
-      }
-      return cleanData;
-    }
-
-    function getExperimentData(titlePath) {
-      for (var i = 0; i < experimentMetaData.length; i++) {
-        if (titlePath == experimentMetaData[i].titlePath) return cleanExperimentData(experimentMetaData[i]);
-      }
-      return null;
+    function uploadFile(filePath, fileContents) {
+      Dropbox.filesUpload({ path: filePath, contents: fileContents, mode: { ".tag": "add" }, autorename: true }).then(function (response) {
+        $log.debug('File Uploaded to Dropbox: ' + JSON.stringify(response));
+        return true;
+      }).catch(function (error) {
+        $log.error(error);
+        return false;
+      });
+      return true;
     }
 
     return factory;
@@ -373,6 +320,59 @@
       }
 
       if (vmObject[textObject][textKey].length == 0) vmObject[wordCountVar] = 0;
+    }
+
+    return factory;
+  }
+})();
+'use strict';
+
+(function () {
+  angular.module('MB').factory('LabService', LabService);
+
+  function LabService() {
+    var factory = {
+      getExperimentMetaData: getExperimentMetaData,
+      getExperimentData: getExperimentData
+    };
+
+    var experimentMetaData = [{
+      titlePath: "glasses",
+      title: "Glasses with Face Recognition",
+      tags: ["Project Sherlock", "Computer Vision"],
+      category: "Computer Vision",
+      preview: "Experimenting with Haar Cascades for real-time face detection."
+    }, {
+      titlePath: "interactive-physics-engine",
+      title: "Interactive Physics Engine",
+      tags: ["Javascript", "Computer Vision", "Physics-Engine"],
+      category: "Computer Vision",
+      preview: "Experimenting with javascript physics engine and hand tracking."
+    }];
+
+    function parseText(text) {
+      // Trim whitespace
+      return text.replace(/^ +| +$/gm, "");
+    }
+
+    function cleanExperimentData(experiment) {
+      experiment.preview = parseText(experiment.preview);
+      return experiment;
+    }
+
+    function getExperimentMetaData() {
+      var cleanData = experimentMetaData;
+      for (var i = 0; i < cleanData.length; i++) {
+        cleanData[i].preview = parseText(cleanData[i].preview);
+      }
+      return cleanData;
+    }
+
+    function getExperimentData(titlePath) {
+      for (var i = 0; i < experimentMetaData.length; i++) {
+        if (titlePath == experimentMetaData[i].titlePath) return cleanExperimentData(experimentMetaData[i]);
+      }
+      return null;
     }
 
     return factory;
